54ca59e8325e5b7d401d398f3319b8c0459ebc8f
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-10-28 22:47:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`مهما كنت صامت فأنا لا أجرح`);
+  client.user.setGame(`نايم|sleep😉`);
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
 });
-
+//مهما كنت صامت فأنا لا أجرح//
 
 client.login(process.env.BOT_TOKEN);
