37c078b27484947d1fe57c05ca77a608f8caae8e
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-10-28 21:07:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`طفشان');
+  client.user.setGame(`طفشان`);
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
