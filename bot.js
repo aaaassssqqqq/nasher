f44bde450b3e0d46b069362e72637dbd4dd83a18
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-11-01 20:47:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`مهما كنت صامت فأنا لا أجرح`);
+  client.user.setGame(`طفشت من الديسكورد | OFF`);
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
