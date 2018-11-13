bcdf4b17423657ef4251dfdb86d2b0a048ec86ac
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-11-13 08:55:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,5 +7,31 @@ client.on('ready', () => {
   console.log('---------------')
 });
 //مهما كنت صامت فأنا لا أجرح
+client.on('message', message => {
+var prefix = "$";
+       if(message.content === prefix + "قفل") {
+                           if(!message.channel.guild) return message.reply('** This command only for servers**');
 
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
+              message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: false
+
+              }).then(() => {
+                  message.reply("**__تم تقفيل الشات__ ✅ **")
+              });
+                }
+//FIRE BOT
+    if(message.content === prefix + "unmutechannel") {
+                        if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
+              message.channel.overwritePermissions(message.guild.id, {
+            SEND_MESSAGES: true
+
+              }).then(() => {
+                  message.reply("**__تم فتح الشات__✅**")
+              });
+    }
+       
+});
 client.login(process.env.BOT_TOKEN);
