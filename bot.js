04923b644123376fc9a6f410babe15e4af28f792
aaaassssqqqq <42968162+aaaassssqqqq@users.noreply.github.com>
2018-11-13 09:10:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,50 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`طفشت من الديسكورد | OFF`);
+  client.user.setGame(`.`);
   console.log('---------------');
   console.log(' Bot Is Online')
   console.log('---------------')
 });
 //مهما كنت صامت فأنا لا أجرح
-client.on('message', message => {
-var prefix = "$";
-       if(message.content === prefix + "قفل") {
-                           if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply(' **__ليس لديك صلاحيات__**');
-              message.channel.overwritePermissions(message.guild.id, {
-            SEND_MESSAGES: false
-
-              }).then(() => {
-                  message.reply("**__تم تقفيل الشات__ ✅ **")
-              });
-                }
-//FIRE BOT
-    if(message.content === prefix + "فك") {
-                        if(!message.channel.guild) return message.reply('** This command only for servers**');
-
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**__ليس لديك صلاحيات__**');
-              message.channel.overwritePermissions(message.guild.id, {
-            SEND_MESSAGES: true
-
-              }).then(() => {
-                  message.reply("**__تم فتح الشات__✅**")
-              });
-    }
-       
-});
-client.on('message', message => {       
-if (message.content.startsWith('$مسح')) { //HoNtar
-    if(!message.channel.guild) return message.reply('⛔ | This Command For Servers Only!'); 
-        if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('⛔ | You dont have **MANAGE_MESSAGES** Permission!');
-        if(!message.guild.member(client.user).hasPermission('MANAGE_MESSAGES')) return message.channel.send('⛔ | I dont have **MANAGE_MESSAGES** Permission!');
- let args = message.content.split(" ").slice(1)
-    let messagecount = parseInt(args);
-    if (args > 1000) return message.reply("**🛑 || يجب ان يكون عدد المسح أقل من 1000 .**").then(messages => messages.delete(5000))
-    if(!messagecount) args = '1000';
-    message.channel.fetchMessages({limit: messagecount + 1}).then(messages => message.channel.bulkDelete(messages));
-    message.channel.send(`\`${args}\` : __عدد الرسائل التي تم مسحها __ `).then(messages => messages.delete(5000));
-  }
-});
 client.login(process.env.BOT_TOKEN);
